da2c655450ce5fcd5ee0c30f2cb1029f73ff9327
Kristof Muhi <mkristof@outlook.com>
2018-06-28 15:17:09 +0200

    rework sample

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const express = require('express');
-const app = express();
-
 var http = require('http');
+
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
@@ -12,15 +10,4 @@ var server = http.createServer(function(request, response) {
 var port = process.env.PORT || 1337;
 server.listen(port);
 
-// serve files from the public directory
-app.use(express.static('public'));
-
-// start the express web server listening on 8080
-app.listen(8080, () => {
-  console.log('listening on 8080');
-});
-
-// serve the homepage
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+console.log("Server running at http://localhost:%d", port);
