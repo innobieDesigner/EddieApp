362d88ac9e76ef04d2c218760c681d0c2cc2baaa
DoctorP96 <40357719+DoctorP96@users.noreply.github.com>
2018-06-28 14:18:14 +0200

    tryit

---
+++ Try/opp.js
@@ -0,0 +1,16 @@
+const express= require('express');
+const mysql=require('mysql');
+
+var fs=require('fs');
+
+console.log('Client-side code running');
+const button = document.getElementById('myButton');
+button.addEventListener('click', function(e) {
+    var obj = {
+        table: []
+     };
+     obj.table.push({id: 1, userID:234, model:"Bio1"});
+     var data = JSON.stringify(obj);
+     //fs.writeFile('MYFILE.json', data);
+     console.log(data)
+});
