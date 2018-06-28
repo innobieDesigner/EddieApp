117b45633a34a6c03f024b77176918bd84ea18bb
Kristof Muhi <mkristof@outlook.com>
2018-06-28 23:40:28 +0200

    sample again

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {}
-
-config.host = process.env.HOST || "https://eddieplatform.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "d1ymY6codnL3iqMdcKhHKXQsNuGzDkYfhuGKzkQx7EwKaFBvlrly9E7xQidD4PDvqtoskP94PS9seVXgkP5DgQ==";
-config.databaseId = "eddieplatform";
-config.collectionId = "model_lists";
-
-module.exports = config;
